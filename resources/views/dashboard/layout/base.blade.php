4f0d8808a57ab8603d0bfd42c60b0d8d0d0676af
Samuel Maculuve <32066945+SamuelMaculuve@users.noreply.github.com>
2024-02-27 21:14:20 +0200

    Fix css block

--- resources/views/dashboard/layout/base.blade.php
+++ resources/views/dashboard/layout/base.blade.php
@@ -5,9 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Contacts</title>
-    <link rel="stylesheet" href="{{ asset('dashboard/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('dashboard/css/style.css') }}" />
-
+    @include('css-flie');
 </head>
 
 <body>
